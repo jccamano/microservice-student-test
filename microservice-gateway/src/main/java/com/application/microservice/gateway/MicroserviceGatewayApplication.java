d15ebf65a0b1f4997abcd44ffb25d73e40dd51a0
Juan Carlos Camano <jucahe27@gmail.com>
2022-08-04 17:40:34 -0500

    se agregan microservicios eureka y gateway

---
+++ microservice-gateway/src/main/java/com/application/microservice/gateway/MicroserviceGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.application.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroserviceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewayApplication.class, args);
+	}
+
+}
